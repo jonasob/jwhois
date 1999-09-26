08ca1f617ce7ff5db5aec1c7330b124f2d09deb7
jonas <jonas>
1999-09-26 10:03:16 +0000

    Readability changes

--- src/jconfig.c
+++ src/jconfig.c
@@ -31,78 +31,91 @@ static struct jconfig *jconfig_ptr = NULL;
 
 void jconfig_set(void)
 {
-	jconfig_tmpptr = jconfig_ptr;
-	return;
+  jconfig_tmpptr = jconfig_ptr;
+  return;
 }
 
 void jconfig_end(void)
 {
-	return;
+  return;
 }
 
-struct jconfig *jconfig_next(char *domain)
+struct jconfig *jconfig_next(domain)
+     char *domain;
 {
-	struct jconfig *ptr;
+  struct jconfig *ptr;
+  
+  if (!jconfig_tmpptr)
+    {
+      return NULL;
+    }
 
-	if (!jconfig_tmpptr) {
-		return NULL;
+  while (jconfig_tmpptr)
+    {
+      if ( (char *)strcasecmp(jconfig_tmpptr->domain, domain) == 0)
+	{
+	  ptr = jconfig_tmpptr;
+	  jconfig_tmpptr = jconfig_tmpptr->next;
+	  return ptr;
 	}
-
-	while (jconfig_tmpptr) {
-		if ( (char *)strstr(jconfig_tmpptr->domain,domain)
-		     == jconfig_tmpptr->domain) {
-			ptr = jconfig_tmpptr;
-			jconfig_tmpptr = jconfig_tmpptr->next;
-			return ptr;
-		}
-		jconfig_tmpptr = jconfig_tmpptr->next;
-	}
-	return NULL;
+      jconfig_tmpptr = jconfig_tmpptr->next;
+    }
+  return NULL;
 }
 
-int jconfig_add(char *domain, char *key, char *value)
+int jconfig_add(domain, key, value, line)
+     char *domain;
+     char *key;
+     char *value;
+     int line;
 {
-	struct jconfig *ptr;
-
-	ptr = malloc(sizeof(struct jconfig));
-	if (!ptr) {
-		return 0;
-	}
-	ptr->key = malloc(strlen(key)+1);
-	if (!ptr->key) {
-		free(ptr);
-		return 0;
-	}
-	ptr->value = malloc(strlen(value)+1);
-	if (!ptr->value) {
-		free(ptr->key);
-		free(ptr);
-		return 0;
-	}
-	ptr->domain = malloc(strlen(domain)+1);
-	if (!ptr->domain) {
-		free(ptr->key);
-		free(ptr->value);
-		free(ptr);
-		return 0;
-	}
-	strncpy(ptr->key, key, strlen(key)+1);
-	strncpy(ptr->value, value, strlen(value)+1);
-	strncpy(ptr->domain, domain, strlen(domain)+1);
-	ptr->next = jconfig_ptr;
-	jconfig_ptr = ptr;
-	return 1;
+  struct jconfig *ptr;
+  
+  ptr = malloc(sizeof(struct jconfig));
+  if (!ptr)
+    {
+      return 0;
+    }
+  ptr->key = malloc(strlen(key)+1);
+  if (!ptr->key)
+    {
+      free(ptr);
+      return 0;
+    }
+  ptr->value = malloc(strlen(value)+1);
+  if (!ptr->value)
+    {
+      free(ptr->key);
+      free(ptr);
+      return 0;
+    }
+  ptr->domain = malloc(strlen(domain)+1);
+  if (!ptr->domain)
+    {
+      free(ptr->key);
+      free(ptr->value);
+      free(ptr);
+      return 0;
+    }
+  strncpy(ptr->key, key, strlen(key)+1);
+  strncpy(ptr->value, value, strlen(value)+1);
+  strncpy(ptr->domain, domain, strlen(domain)+1);
+  ptr->line = line;
+  ptr->next = jconfig_ptr;
+  jconfig_ptr = ptr;
+  return 1;
 }
 
 void jconfig_free(void)
 {
-	struct jconfig *ptr;
-
-	while (jconfig_ptr) {
-		free(jconfig_ptr->value);
-		free(jconfig_ptr->key);
-		ptr = jconfig_ptr;
-		jconfig_ptr = jconfig_ptr->next;
-		free(ptr);
-	}
+  struct jconfig *ptr;
+  
+  while (jconfig_ptr)
+    {
+      free(jconfig_ptr->value);
+      free(jconfig_ptr->key);
+      ptr = jconfig_ptr;
+      jconfig_ptr = jconfig_ptr->next;
+      free(ptr);
+    }
 }
--- src/jwhois.c
+++ src/jwhois.c
@@ -51,202 +51,234 @@
 #define DO_VERSION 0x02
 static struct option long_options[] = 
 {
-	{"version", 0, 0, DO_VERSION},
-	{"help", 0, 0, DO_HELP},
-	{"config", 1, 0, 'c'},
-	{"host", 1, 0, 'h'},
-	{"port", 1, 0, 'p'},
-	{0, 0, 0, 0}
+  {"version", 0, 0, DO_VERSION},
+  {"help", 0, 0, DO_HELP},
+  {"config", 1, 0, 'c'},
+  {"host", 1, 0, 'h'},
+  {"port", 1, 0, 'p'},
+  {0, 0, 0, 0}
 };
 
 void help(void)
 {
-	printf("%s%s%s%s%s%s%s%s%s%s%s%s", PACKAGE, " version ", VERSION,
-		", Copyright (C) 1999 Jonas Öberg\n",
-		"This is free software with ABSOLUTELY NO WARRANTY.\n\n",
-		"Usage: jwhois [OPTIONS] [QUERIES...]\n",
-		"  --version               display version number and patch level\n",
-		"  --help                  display this help\n",
-                "  -c FILE, --config=FILE  use FILE as configuration file\n",
-		"  -h HOST, --host=HOST    explicitly query HOST\n",
-		"  -p PORT, --port=PORT    use port number PORT (in conjunction with HOST)\n",
-		"\n\nReport bugs to jonas@coyote.org\n");
+  printf("%s%s%s%s%s%s%s%s%s%s%s%s", PACKAGE, " version ", VERSION,
+	 ", Copyright (C) 1999 Jonas Öberg\n",
+	 "This is free software with ABSOLUTELY NO WARRANTY.\n\n",
+	 "Usage: jwhois [OPTIONS] [QUERIES...]\n",
+	 "  --version               display version number and patch level\n",
+	 "  --help                  display this help\n",
+	 "  -c FILE, --config=FILE  use FILE as configuration file\n",
+	 "  -h HOST, --host=HOST    explicitly query HOST\n",
+	 "  -p PORT, --port=PORT    use port number PORT (in conjunction with HOST)\n",
+	 "\n\nReport bugs to jonas@coyote.org\n");
 }
 
-int query_host(char *host, int port, char *val)
+int query_host(host, port, val)
+     char *host;
+     int port;
+     char *val;
 {
-	int sockfd, ret;
-	struct protoent *pent;
-	struct sockaddr_in remote;
-	struct hostent *hostent;
-	char *command;
+  int sockfd, ret;
+  struct protoent *pent;
+  struct sockaddr_in remote;
+  struct hostent *hostent;
+  char *command;
 
-	command = malloc(MAXBUFSIZE);
-	if (!command) {
-		perror("");
-		exit(1);
-	}
-	remote.sin_family = AF_INET;
-	remote.sin_port = port;
+  command = malloc(MAXBUFSIZE);
+  if (!command)
+    {
+      perror("");
+      exit(1);
+    }
+  remote.sin_family = AF_INET;
+  remote.sin_port = port;
 #ifdef HAVE_INET_ATON
-	ret = inet_aton(host, &remote.sin_addr.s_addr);
+  ret = inet_aton(host, &remote.sin_addr.s_addr);
 #else
-	remote.sin_addr.s_addr = inet_addr(host);
-	if (remote.sin_addr.s_addr == -1)
-		ret = 0;
+  remote.sin_addr.s_addr = inet_addr(host);
+  if (remote.sin_addr.s_addr == -1)
+    ret = 0;
 #endif
-	if (!ret) {
-		hostent = gethostbyname(host);
-		if (!hostent) {
-			fprintf(stderr, "%s: host unknown\n", host);
-			exit(1);
-		}
-		memcpy(&remote.sin_addr.s_addr, hostent->h_addr_list[0],
-			sizeof(remote.sin_addr.s_addr));
+  if (!ret)
+    {
+      hostent = gethostbyname(host);
+      if (!hostent)
+	{
+	  fprintf(stderr, "%s: host unknown\n", host);
+	  exit(1);
 	}
-	sockfd = socket(AF_INET, SOCK_STREAM, IPPROTO_IP);
-	if (!sockfd) {
-		perror("Can not create socket");
-		exit(1);
-	}
-	ret = connect(sockfd, (struct sockaddr *)&remote, sizeof(struct sockaddr));
-	if (ret == -1) {
-		perror(host);
-		exit(1);
-	}
-	fprintf(stderr, "[%s]\n", host);
-	write(sockfd, val, strlen(val));
-	write(sockfd, "\r\n", 2);
-	while (ret = read(sockfd, command, MAXBUFSIZE)) {
-		fwrite(command, ret, 1, stdout);
-	}
-	close(sockfd);
+      memcpy(&remote.sin_addr.s_addr, hostent->h_addr_list[0],
+	     sizeof(remote.sin_addr.s_addr));
+    }
+
+  sockfd = socket(AF_INET, SOCK_STREAM, IPPROTO_IP);
+  if (!sockfd)
+    {
+      perror("Can not create socket");
+      exit(1);
+    }
+  ret = connect(sockfd, (struct sockaddr *)&remote, sizeof(struct sockaddr));
+  if (ret == -1)
+    {
+      perror(host);
+      exit(1);
+    }
+  fprintf(stderr, "[%s]\n", host);
+  write(sockfd, val, strlen(val));
+  write(sockfd, "\r\n", 2);
+  while (ret = read(sockfd, command, MAXBUFSIZE))
+    {
+      fwrite(command, ret, 1, stdout);
+    }
+  close(sockfd);
 }
 
-void make_query(char *val)
+void make_query(val)
+     char *val;
 {
-	struct jconfig *j;
-	struct re_pattern_buffer	rpb;
-	char *error, *host = DEFAULTHOST, *ret, *tmphost;
-	int ind, port;
+  struct jconfig *j;
+  struct re_pattern_buffer	rpb;
+  char *error, *host = DEFAULTHOST, *ret, *tmphost;
+  int ind, port;
 
-	jconfig_set();
-	while (j = jconfig_next("jwhois.whois-servers")) {
-		rpb.allocated = 0;
-		rpb.buffer = (unsigned char *)NULL;
-		rpb.translate = rpb.fastmap = (char *)NULL;
-		if (error = (char *)re_compile_pattern(j->key, strlen(j->key),
-			&rpb)) {
-			perror(error);
-			exit(1);
-		}
-		ind = re_search(&rpb, val, strlen(val), 0, 0, NULL);
-		if (ind == 0) {
-			host = j->value;
-		} else if (ind == -2) {
-			fprintf(stderr, "re_search internal error\n");
-			exit(1);
-		}
+  jconfig_set();
+  while (j = jconfig_next("jwhois.whois-servers"))
+    {
+      rpb.allocated = 0;
+      rpb.buffer = (unsigned char *)NULL;
+      rpb.translate = rpb.fastmap = (char *)NULL;
+      if (error = (char *)re_compile_pattern(j->key, strlen(j->key), &rpb))
+	{
+	  perror(error);
+	  exit(1);
 	}
-	jconfig_end();
-
-	port = htons(IPPORT_WHOIS);
-	if (strchr(host, ':')) {
-		tmphost = (char *)strchr(host, ':');
+      ind = re_search(&rpb, val, strlen(val), 0, 0, NULL);
+      if (ind == 0)
+	{
+	  host = j->value;
+	}
+      else if (ind == -2)
+	{
+	  fprintf(stderr, "re_search internal error\n");
+	  exit(1);
+	}
+    }
+  jconfig_end();
+  
+  port = htons(IPPORT_WHOIS);
+  if (strchr(host, ':'))
+    {
+      tmphost = (char *)strchr(host, ':');
 #ifdef HAVE_STRTOL
-		port = htons(strtol((char *)(tmphost+1), &ret, 10));
-		if (*ret != '\0') {
-			fprintf(stderr, "%s: %s %s %s\n",
-				PACKAGE,
-				"Invalid port number for host",
-				host,
-				"in config file");
-			exit(1);
-		}
-#else
-		port = htons(atoi((char *)(tmphost+1)));
-#endif
-		*tmphost = '\0';
+      port = htons(strtol((char *)(tmphost+1), &ret, 10));
+      if (*ret != '\0')
+	{
+	  fprintf(stderr, "%s: %s %s %s\n",
+		  PACKAGE,
+		  "Invalid port number for host",
+		  host,
+		  "in config file");
+	  exit(1);
 	}
-	query_host(host,port,val);
-	return;
+#else
+      port = htons(atoi((char *)(tmphost+1)));
+#endif
+      *tmphost = '\0';
+    }
+  query_host(host,port,val);
+  return;
 }
 
-int main(int argc, char **argv)
+int main(argc, argv)
+     int argc;
+     char **argv;
 {
-	int optch, option_index, port;
-	char *config = NULL, *host = NULL, *errmsg, *ret;
-	FILE *in;
-
-	port = htons(IPPORT_WHOIS);
-
-	while (1) {
-		optch = getopt_long(argc, argv, "c:h:p:", long_options, &option_index);
-		if (optch == EOF)
-			break;
-
-		switch (optch) {
-			case DO_VERSION:
-				printf("%s %s\n", PACKAGE, VERSION);
-				exit(0);
-			case DO_HELP:
-				help();
-				exit(0);
-			case 'c':
-				if (config) free(config);
-				config = malloc(strlen(optarg)+1);
-				strncpy(config, optarg, strlen(optarg)+1);
-				break;
-			case 'h':
-				if (host) free(host);
-				host = malloc(strlen(optarg)+1);
-				strncpy(host, optarg, strlen(optarg)+1);					break;
-			case 'p':
+  int optch, option_index, port;
+  char *config = NULL, *host = NULL, *errmsg, *ret;
+  FILE *in;
+  
+  port = htons(IPPORT_WHOIS);
+  
+  while (1)
+    {
+      optch = getopt_long(argc, argv, "c:h:p:", long_options, &option_index);
+      if (optch == EOF)
+	break;
+      
+      switch (optch)
+	{
+	case DO_VERSION:
+	  printf("%s %s\n", PACKAGE, VERSION);
+	  exit(0);
+	case DO_HELP:
+	  help();
+	  exit(0);
+	case 'c':
+	  if (config) free(config);
+	  config = malloc(strlen(optarg)+1);
+	  strncpy(config, optarg, strlen(optarg)+1);
+	  break;
+	case 'h':
+	  if (host) free(host);
+	  host = malloc(strlen(optarg)+1);
+	  strncpy(host, optarg, strlen(optarg)+1);					break;
+	case 'p':
 #ifdef HAVE_STRTOL
-				port = htons(strtol(optarg, &ret, 10));
-				if (*ret != '\0') {
-					fprintf(stderr, "%s: %s (%s)\n",
-						PACKAGE,
-						"Invalid port number",
-						optarg);
-					exit(1);
-				}
+	  port = htons(strtol(optarg, &ret, 10));
+	  if (*ret != '\0')
+	    {
+	      fprintf(stderr, "%s: %s (%s)\n",
+		      PACKAGE,
+		      "Invalid port number",
+		      optarg);
+	      exit(1);
+	    }
 #else
-				port = htons(atoi(optarg));
+	  port = htons(atoi(optarg));
 #endif
-				break;
-		}
+	  break;
 	}
+    }
 
-	if (optind == argc) {
-		help();
-		exit(0);
+  if (optind == argc)
+    {
+      help();
+      exit(0);
+    }
+
+  if (config)
+    {
+      in = fopen(config, "r");
+      if (!in)
+	{
+	  perror(config);
+	  exit(1);
 	}
-
-	if (config) {
-		in = fopen(config, "r");
-		if (!in) {
-			perror(config);
-			exit(1);
-		}
-	} else {
-		in = fopen(SYSCONFDIR "/jwhois.conf", "r");
-		if (!in) {
-		  in = fopen(DATADIR "/jwhois.conf", "r");
-		}
+    }
+  else
+    {
+      in = fopen(SYSCONFDIR "/jwhois.conf", "r");
+      if (!in)
+	{
+	  in = fopen(DATADIR "/jwhois.conf", "r");
 	}
-	if (in)
-		parse_config(in);
+    }
+  if (in)
+    parse_config(in);
 
-	re_syntax_options = RE_SYNTAX_EMACS;
-
-	while (optind < argc) {
-		if (host) {
-			query_host(host, port, argv[optind++]);
-		} else {
-			make_query(argv[optind++]);
-		}
+  re_syntax_options = RE_SYNTAX_EMACS;
+  
+  while (optind < argc)
+    {
+      if (host)
+	{
+	  query_host(host, port, argv[optind++]);
 	}
-
-	return 0;
+      else
+	{
+	  make_query(argv[optind++]);
+	}
+    }
+  
+  return 0;
 }
